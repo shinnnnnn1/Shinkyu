3117c81c3dfd5ce638c13e44809212bce890ae28
shinnnnnn2 <shindy0224@gmail.com>
2023-12-13 17:51:57 +0900

    Player Control - Move

---
+++ Assets/Shinnnnnn1/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instancee = FindObjectOfType<GameManager>();
+            }
+            return instancee;
+        }
+    }
+    private static GameManager instancee;
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Shinnnnnn1/Scripts/PlayerCtrl.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    Rigidbody rigid;
+    [SerializeField] [Range(0, 3)] float moveSpd;
+    [SerializeField] float jumpPow;
+    [SerializeField] float sensitivity;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Jump();
+    }
+    void FixedUpdate()
+    {
+        Move();
+        RotateY();
+        RotateX();
+    }
+
+    void Move()
+    {
+        Vector3 hor = transform.right * Input.GetAxisRaw("Horizontal");
+        Vector3 ver = transform.forward * Input.GetAxisRaw("Vertical");
+        Vector3 veloc = (hor + ver).normalized * moveSpd * 0.2f;
+        rigid.MovePosition(transform.position + veloc);
+    }
+
+    void RotateY()
+    {
+
+    }
+
+    void RotateX()
+    {
+
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector3.up * jumpPow * 100);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+
+    }
+}
